1e76cffe4022a8f43c3f216a55a8bca8b9e6e802
H4ckint0sh <alijan.adeli@icloud.com>
2024-11-09 21:24:51 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold underline">Welcome to Car Rental</h1>
+    </div>
+  );
+}
+
+export default App;
